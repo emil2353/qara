e0d1377a5004d4b5df27ff9a7145d7e65c808704
Emil <emil2353@mail.ru>
2020-01-10 11:49:52 +0400

    this is first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+alert("Hello world");
+
+let message;
+message='salam';
+alert(message);
+
+console.log('emil privet');
+
+var persone = {
+  name: 'Emil',
+  age: 34,
+  isMarried: true
+};
+
+// console.log(persone.name);
+// console.log(persone['age']);
+
+// var arr = ['emil', '1', 'k.jpg'];
+// console.log(arr[1]);
+
+let inc = 10,
+    dec = 11;
+
+    console.log("inc");
+
+let num = 10;
+  while (num<15){
+    console.log(num);
+    num++;
+}
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let message ='gasimov';
+alert (message);
+
+let message = 'Hello!'; // определяем переменную и присваиваем ей значение
+
+alert(message); // Hello!
+
+
+console.log("emil");
